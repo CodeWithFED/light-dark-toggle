fa8d3cd252f3eaaed7cf9ae5056167bab921df70
Erfan <Codewithfed@gmail.com>
2024-01-12 14:56:28 +0330

    removing duplications in code

--- extra/index.js
+++ extra/index.js
@@ -1,18 +1,6 @@
-let toggle = document.querySelector('.icon-container');
-let btn = document.querySelector('.fa-moon');
+const toggle = document.querySelector('.icon-container');
 let body = document.body;
-let numbertimes = 0;
-toggle.addEventListener('click', () => {
-    numbertimes++;
-    if (numbertimes % 2 == 0) {
-        btn.classList.add('fa-solid');
-        body.style.background = '#f9f9f9';
-        body.style.color = '#222';
-    }
 
-    else {
-        btn.classList.remove('fa-solid')
-        body.style.background = '#222';
-        body.style.color = '#f9f9f9';
-    }
-})
+toggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
